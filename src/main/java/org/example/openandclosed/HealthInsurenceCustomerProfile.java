3eaa9295da1e802ba4b7689c2beccbc158c6c4de
Akash Yadagouda <akash@Akashs-MacBook-Air.local>
2024-09-07 00:13:43 +0530

    added comments

--- src/main/java/org/example/openandclosed/HealthInsurenceCustomerProfile.java
+++ src/main/java/org/example/openandclosed/HealthInsurenceCustomerProfile.java
@@ -1,5 +1,5 @@
 package org.example.openandclosed;
-
+// this is for usecase
 public class HealthInsurenceCustomerProfile implements CustomerProfile {
     @Override
     public boolean isGoodCustomer() {
